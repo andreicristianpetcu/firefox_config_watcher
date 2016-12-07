114ddc1c04c546b19335e5c3a28db6ce0ed85c68
Andrei Cristian Petcu <andrei@ceata.org>
2016-12-07 21:34:33 +0200

    Wed Dec  7 21:34:33 EET 2016

---
+++ check_changes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+wget https://hg.mozilla.org/mozilla-central/raw-file/tip/modules/libpref/init/all.js -O nightly_libpref.all.js
+wget https://hg.mozilla.org/releases/mozilla-aurora/raw-file/tip/modules/libpref/init/all.js -O aurora_libpref.all.js
+wget https://hg.mozilla.org/releases/mozilla-beta/raw-file/tip/modules/libpref/init/all.js -O beta_libpref.all.js
+wget https://hg.mozilla.org/releases/mozilla-release/raw-file/tip/modules/libpref/init/all.js -O stable_libpref.all.js
+
+wget https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/app/profile/firefox.js -O nightly_profile.all.js
+wget https://hg.mozilla.org/releases/mozilla-aurora/raw-file/tip/browser/app/profile/firefox.js -O aurora_profile.all.js
+wget https://hg.mozilla.org/releases/mozilla-beta/raw-file/tip/browser/app/profile/firefox.js -O beta_profile.all.js
+wget https://hg.mozilla.org/releases/mozilla-release/raw-file/tip/browser/app/profile/firefox.js -O stable_profile.all.js
