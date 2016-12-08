8094a60033479cd6849ce892e715e5ca8b1d56ff
Andrei Cristian Petcu <andrei@ceata.org>
2016-12-09 00:39:28 +0200

    Prefs changed at Fri Dec  9 00:39:28 EET 2016

--- check_changes.sh
+++ check_changes.sh
@@ -8,3 +8,7 @@ wget https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/app/profile/fir
 wget https://hg.mozilla.org/releases/mozilla-aurora/raw-file/tip/browser/app/profile/firefox.js -O aurora_profile.all.js
 wget https://hg.mozilla.org/releases/mozilla-beta/raw-file/tip/browser/app/profile/firefox.js -O beta_profile.all.js
 wget https://hg.mozilla.org/releases/mozilla-release/raw-file/tip/browser/app/profile/firefox.js -O stable_profile.all.js
+
+git status
+
+[ "`git status -s | wc -l`" -gt "0" ] && git add -A . && git commit --no-gpg-sign -m "Prefs changed at `date`"
