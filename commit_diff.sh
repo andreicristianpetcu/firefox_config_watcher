4bbe6ba2c02e18c98fefc1745a92e0e81496b87b
Andrei Cristian Petcu <andrei.petcu@connectis.nl>
2016-12-12 10:16:52 +0200

    separate check and commit

--- commit_diff.sh
+++ commit_diff.sh
@@ -10,3 +10,5 @@ wget https://hg.mozilla.org/releases/mozilla-beta/raw-file/tip/browser/app/profi
 wget https://hg.mozilla.org/releases/mozilla-release/raw-file/tip/browser/app/profile/firefox.js -O stable_profile.all.js
 
 git status
+
+[ "`git status -s | wc -l`" -gt "0" ] && git add -A . && git commit --no-gpg-sign -m "Prefs changed at `date`"
